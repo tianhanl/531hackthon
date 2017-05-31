c49783c804ab606f2589f74a0ff29462fe2311e8
Tianhang Liu <tianhanl@usc.edu>
2017-05-31 23:00:28 +0800

    Change cards to using distinctive card component

---
+++ 531Project/WebContent/script/cards.js
@@ -0,0 +1,24 @@
+
+const ZhihuCardContent = {
+  template: '<div class="card-head"><img class="card-icon" :src="./img/zhihu-icon.jpg" alt="icon"/><h2 class="card-name">Zhihu Daily</h2></div> <div class="card-content" ><a v-for="article in articleList" :src="article.url">{{title}}</a></div></div>',
+  computed: {
+    articleList: function () {
+      return [{
+        title: "test",
+        url: 'https://daily.zhihu.com/story/94509443930883'
+      }];
+    }
+  }
+};
+
+const ClockCardContent = {
+  template: '<div class="card" > <div class="card-head"><img class="card-icon" :src="./img/clock-icon.jpg" alt="icon"/><h2 class="card-name">Clock</h2></div> <div class="card-content" >{{currentTime}}</div>',
+  computed: {
+    currentTime: function () {
+      return '11:15';
+    }
+  }
+};
+
+Vue.component('zhihu-card-content', ZhihuCardContent);
+vue.component('clock-card-content', ClockCardContent);

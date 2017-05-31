b629a0baa1fcfd2c068b9c291429268ba69c2dc5
Tianhang Liu <tianhanl@usc.edu>
2017-05-31 17:24:37 +0800

    add mock up of the vue application

---
+++ 531Project/WebContent/userProfile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-pane">
+    < div class="user-profile">
+      <img v-bind:src="avatarLink" class="avatar" alt="user-avatar" />
+      <h3 class="user-name">{{ name }}</h3>
+  </div>
+  <p id="display">
+    {{ display }}
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userProfile',
+  props: {
+    name: string,
+    avatarLink: string
+  },
+  computed: {
+    display: function () {
+      return test;
+    }
+  }
+}
+</script>

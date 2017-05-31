5768fa978c0b1f53f618a04fda57091542ae6a99
Tianhang Liu <tianhanl@usc.edu>
2017-06-01 02:11:15 +0800

    add primitive impleentation of clock

--- 531Project/WebContent/script/cards.js
+++ 531Project/WebContent/script/cards.js
@@ -1,24 +1,79 @@
-
 const ZhihuCardContent = {
-  template: '<div class="card-head"><h2 class="card-name">Zhihu Daily</h2></div> <div class="card-content" ><a v-for="article in articleList" :src="article.url">{{title}}</a></div></div>',
+  template: '<div><div class="card-head"><h2 class="card-name">Zhihu Daily</h2></div> <div class="card-content" ><h3>Top Stories:</h3><a v-for="article in articleList" :href="article.url" target="_blank">{{article.title}}</a></div></div>',
   computed: {
     articleList: function () {
-      return [{
-        title: "test",
-        url: 'https://daily.zhihu.com/story/94509443930883'
-      }];
+      var list = [];
+      var resData = {
+        date: "20140523",
+        stories: [
+          {
+            title: "中国古代家具发展到今天有两个高峰，一个两宋一个明末（多图）",
+            ga_prefix: "052321",
+            images: [
+              "http://p1.zhimg.com/45/b9/45b9f057fc1957ed2c946814342c0f02.jpg"
+            ],
+            type: 0,
+            id: 3930445
+          },
+        ],
+        top_stories: [
+          {
+            title: "商场和很多人家里，竹制家具越来越多（多图）",
+            image: "http://p2.zhimg.com/9a/15/9a1570bb9e5fa53ae9fb9269a56ee019.jpg",
+            ga_prefix: "052315",
+            type: 0,
+            id: 3930883
+          }
+        ]
+      };
+      var topStories = resData.top_stories;
+      topStories.forEach(function (element) {
+        let story = {};
+        story.title = element.title;
+        story.url = 'https://daily.zhihu.com/story/' + element.id;
+        list.push(story);
+      });
+
+      return list;
     }
   }
 };
 
 const ClockCardContent = {
-  template: '<div class="card" > <div class="card-head"><h2 class="card-name">Clock</h2></div> <div class="card-content" >{{currentTime}}</div>',
+  template: '<div><div class="card-head"><h2 class="card-name">Clock</h2></div> <div class="card-content" >{{currentTime}}</div></div>',
+  data: function () {
+    return {
+      currentHour: new Date().getHours(),
+      currentMinute: new Date().getMinutes()
+    };
+  },
   computed: {
     currentTime: function () {
-      return '11:15';
+      return '' + this.currentHour + ':' + this.currentMinute;
     }
+  },
+  methods: {
+    setUpdateTime: function () {
+      var that = this;
+      setInterval(function () {
+        that.updateTime();
+      }, 60000);
+    },
+    updateTime: function () {
+      this.currentMinute++;
+      if (this.currentMinute >= 60) {
+        this.currentMinute -= 60;
+        this.currentHour++;
+        if (this.currentHour >= 24) {
+          this.currentHour -= 24;
+        }
+      }
+    }
+  },
+  created: function () {
+    this.setUpdateTime();
   }
 };
 
 Vue.component('zhihu-card-content', ZhihuCardContent);
-vue.component('clock-card-content', ClockCardContent);
+Vue.component('clock-card-content', ClockCardContent);

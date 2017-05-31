836a90b61ec17658ba568418bb264919a85480cb
YanboChen <YanboChen@yyl.local>
2017-05-31 18:14:14 +0800

    Usr & card class added

---
+++ 531Project/src/data/Card.java
@@ -0,0 +1,7 @@
+package data;
+
+public class Card {
+	public final static String ZHIHU = "zhihudaily";
+	public final static String WEATHER = "weather";
+	public final static String RTC = "RTC";
+}
---
+++ 531Project/src/data/User.java
@@ -0,0 +1,56 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class User {
+	private String username;
+	private String password;
+	private String nickname;
+	private List<String> cardlist = new ArrayList<String>();
+	
+	
+	public User(){
+		
+	}
+	
+	public User(String usr, String pwd, String nck){
+		this.setUsername(usr);
+		this.setPassword(pwd);
+		this.setNickname(nck);
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public List<String> getCardlist() {
+		return cardlist;
+	}
+	
+	public void addOneCard(){
+		
+	}
+	
+}

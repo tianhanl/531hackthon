b18bba191536540c88dd9c01f929c3917aac3fe6
Tianhang Liu <tianhanl@usc.edu>
2017-06-01 15:01:03 +0800

    using custom zhihu API for zhihu card

--- 531Project/WebContent/script/cards.js
+++ 531Project/WebContent/script/cards.js
@@ -19,29 +19,12 @@ const ZhihuCardContent = {
   computed: {
     articleList: function () {
       var list = [];
-      var resData = {
-        date: "20140523",
-        stories: [
-          {
-            title: "中国古代家具发展到今天有两个高峰，一个两宋一个明末（多图）",
-            ga_prefix: "052321",
-            images: [
-              "http://p1.zhimg.com/45/b9/45b9f057fc1957ed2c946814342c0f02.jpg"
-            ],
-            type: 0,
-            id: 3930445
-          },
-        ],
-        top_stories: [
-          {
-            title: "商场和很多人家里，竹制家具越来越多（多图）",
-            image: "http://p2.zhimg.com/9a/15/9a1570bb9e5fa53ae9fb9269a56ee019.jpg",
-            ga_prefix: "052315",
-            type: 0,
-            id: 3930883
-          }
-        ]
-      };
+      var resData;
+      axios.get('/actionServlet').then(function (response) {
+        resData = JSON.parse(response.data);
+      }).catch(function (error) {
+        console.log(error);
+      });
       var topStories = resData.top_stories;
       topStories.forEach(function (element) {
         let story = {};
